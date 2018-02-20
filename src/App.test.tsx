d680e9f7f09b3a44fdcd9693119e79a4dc280157
MasuqaT <occar@hotmail.co.jp>
2018-02-20 21:45:40 +0900

    Introduce Enzyme.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,19 @@
+import * as enzyme from 'enzyme';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+    const div = document.createElement('div');
+    ReactDOM.render(<App/>, div);
+});
+
+it('renders the header', () => {
+    const app = enzyme.shallow(<App/>);
+    expect(app.find('.App-title')).toBeDefined();
+});
+
+it('renders the description', () => {
+    const app = enzyme.shallow(<App/>);
+    expect(app.find('.App-intro')).toBeDefined();
 });

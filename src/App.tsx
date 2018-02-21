383c507d979aa3c2f35c4099271f094a50d190e5
MasuqaT <occar@hotmail.co.jp>
2018-02-21 23:53:17 +0900

    Add CSS Typing support.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import * as React from 'react';
-import './App.css';
+import * as style from './App.css';
 
 import logo from './logo.svg';
 
 class App extends React.Component {
     public render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h1 className="App-title">Welcome to React</h1>
+            <div className={style.app}>
+                <header className={style.appHeader}>
+                    <img src={logo} className={style.appLogo} alt="logo"/>
+                    <h1 className={style.appTitle}>Welcome to React</h1>
                 </header>
-                <p className="App-intro">
+                <p className={style.appIntro}>
                     To get started, edit <code>src/App.tsx</code> and save to reload.
                 </p>
             </div>
